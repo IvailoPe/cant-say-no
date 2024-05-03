3a53e891d9a9ebb50e41bb07689099a70c0901ca
IvailoPe <Ivailo.greta@gmail.com>
2024-05-04 01:15:40 +0300

    removing some things

--- index.js
+++ index.js
@@ -8,35 +8,25 @@ document.querySelector("#no-btn").addEventListener("mouseover", (e) => {
         }
     }
     let btn = e.currentTarget
-    if(!btn.style.position){
+    if (!btn.style.position) {
         btn.style.position = "relative"
     }
-    console.log(btn.style.position);
-    console.log(btnCordinates);
-    console.log(e);
-    console.log(`mouse x ${mouseX}`);
-    console.log(`mouse y ${mouseY}`);
     if (mouseY < btnCordinates.bottom && mouseX >= btnCordinates.left && mouseX - btnCordinates.left <= 10) {
-        console.log("lqvo");
-        btn.style.left = +btn.style.left.replace("px","") + 70 + "px";
+        btn.style.left = +btn.style.left.replace("px", "") + 70 + "px";
     }
-    else if(mouseY < btnCordinates.bottom && mouseX <= btnCordinates.right && btnCordinates.right - mouseX <= 10){
-        console.log("dqsno");
-        btn.style.left = +btn.style.left.replace("px","") - 70 + "px";
+    else if (mouseY < btnCordinates.bottom && mouseX <= btnCordinates.right && btnCordinates.right - mouseX <= 10) {
+        btn.style.left = +btn.style.left.replace("px", "") - 70 + "px";
     }
-    else if(mouseY >= btnCordinates.bottom || btnCordinates.bottom - mouseY <= 15){
-      //  console.log("dolu");
-        btn.style.top = +btn.style.top.replace("px","") - 70 + "px";
+    else if (mouseY >= btnCordinates.bottom || btnCordinates.bottom - mouseY <= 15) {
+        btn.style.top = +btn.style.top.replace("px", "") - 70 + "px";
     }
-    else if(mouseY < btnCordinates.bottom){
-       // console.log("gore");
-        btn.style.top = +btn.style.top.replace("px","") + 70 + "px";
+    else if (mouseY < btnCordinates.bottom) {
+        btn.style.top = +btn.style.top.replace("px", "") + 70 + "px";
     }
-    //console.log(btn.style);
 })
 
-document.addEventListener("scroll",() => {
-    if(document.querySelector("#no-btn")){
+document.addEventListener("scroll", () => {
+    if (document.querySelector("#no-btn")) {
         document.querySelector("#no-btn").remove();
     }
 })
